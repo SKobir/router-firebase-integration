122c5c09113ce75a13d713519bbfcb636abf18ad
SKobir <kobir0058bd@gmail.com>
2022-04-15 01:05:51 +0600

    google sign in done

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Routes } from 'react-router-dom';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
+import Register from './components/Register/Register';
 
 function App() {
   return (
@@ -11,7 +12,8 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path="/" element= {<Home></Home>}>Home</Route>
-        <Route path="/" element= {<Login></Login>}>Login</Route>
+        <Route path="/login" element= {<Login></Login>}>Login</Route>
+        <Route path='/register' element={<Register></Register>}></Route>
       </Routes>
     </div>
   );
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import useFirebase from '../../hooks/Hooks';
+
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase();
     return (
         <div>
-            <h2>This is Login Page</h2>
+            <h2>Please Login</h2>
+            <div style={{ margin: '20px' }}>
+                <button onClick={signInWithGoogle}>Google Sign In</button>
+            </div>
+
+            <form>
+                <input type="email" name="" id="" placeholder='Email' />
+                <br />
+                <input type="password" name="" id="" placeholder='Password' />
+                <br />
+                <input type="submit" value="Login" />
+            </form>
         </div>
     );
 };
